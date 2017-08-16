e5c5480dc90471f8d309c74dbeb1197e0110b156
adrien <adrien.ysnel@compte-nickel.fr>
2017-08-16 17:13:44 +0200

    links to access all features

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,22 +11,12 @@ import play.api.mvc._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, ws : WSClient)
                               (airportRepository: AirportRepository,
                                countryRepository: CountryRepository,
                               runwayRepository: RunwayRepository) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
@@ -39,6 +29,10 @@ class HomeController @Inject()(cc: ControllerComponents, ws : WSClient)
 
   def getForm = Action { implicit request =>
     Ok(views.html.form())
+  }
+
+  def getReport = Action {
+    Ok(views.html.report())
   }
 
   def findAirports = Action { implicit request =>
