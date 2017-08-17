7a800a81c3eef8ebe4d93fbdea0a508ce5cf81f8
adrien <adrien.ysnel@compte-nickel.fr>
2017-08-17 10:49:17 +0200

    routes tested

--- test/FunctionalSpec.scala
+++ test/FunctionalSpec.scala
@@ -1,8 +1,9 @@
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.http.Status
+import play.api.http.{Status}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import play.api.test.CSRFTokenHelper._
 
 /**
  * Functional tests start a Play application internally, available
@@ -18,6 +19,16 @@ class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
 
     "send 200 on a good request" in  {
       route(app, FakeRequest(GET, "/")).map(status(_)) mustBe Some(OK)
+      route(app, FakeRequest(GET, "/reports")).map(status(_)) mustBe Some(OK)
+      route(app, FakeRequest(GET, "/countries/airports")).map(status(_)) mustBe Some(OK)
+      route(app, FakeRequest(GET, "/countries/runways")).map(status(_)) mustBe Some(OK)
+      route(app, FakeRequest(GET, "/form")).map(status(_)) mustBe Some(OK)
+    }
+    "send 303 on a good request" in {
+      route(app, FakeRequest(POST, "/airports").withFormUrlEncodedBody("input" -> "RU").withCSRFToken).map(status(_)) mustBe Some(SEE_OTHER)
+    }
+    "send 400 on a bad request" in {
+      route(app, FakeRequest(POST, "/airports").withFormUrlEncodedBody("input" -> "").withCSRFToken).map(status(_)) mustBe Some(BAD_REQUEST)
     }
 
   }
@@ -29,17 +40,7 @@ class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
 
       status(home) mustBe Status.OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Your new application is ready.")
-    }
-
-  }
-
-  "CountController" should {
-
-    "return an increasing count" in {
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "0"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "1"
-      contentAsString(route(app, FakeRequest(GET, "/count")).get) mustBe "2"
+      contentAsString(home) must include ("Index")
     }
 
   }
